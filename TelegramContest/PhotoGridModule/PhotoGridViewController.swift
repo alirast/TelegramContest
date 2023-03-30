3d65bd708b74c59ec408e3a21f513f13cf56fc26
alirast <xdabnormal@gmail.com>
2023-03-30 22:34:56 +0300

    PhotoGridViewController setup

--- TelegramContest/PhotoGridModule/PhotoGridViewController.swift
+++ TelegramContest/PhotoGridModule/PhotoGridViewController.swift
@@ -11,10 +11,24 @@ import Photos
 class PhotoGridViewController: UIViewController {
     var imageArray = [UIImage]()
 //FIXME: collection view setting up
-    let collection = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+    let collectionView = UICollectionView(frame: UIScreen.main.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+    
+//MARK: - viewDidLoad
+//FIXME: - setting ui not in view did load
+    override func viewDidLoad() {
+        collectionView.backgroundColor = .black
+        view.addSubview(collectionView)
+        getPhotos()
+    }
     
 //MARK: - getPhotos
     func getPhotos() {
         let imageManager = PHImageManager.default()
+    }
+//MARK: - openPrivacySettings
+//FIXME: - details?
+    func openPrivacySettings() {
+        guard let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) else { assertionFailure("Not able to open app privacy settings."); return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
 }
