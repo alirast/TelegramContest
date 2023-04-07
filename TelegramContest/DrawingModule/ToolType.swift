0cb87c2b9413e777c68bb5a095d7b57b88cd5914
alirast <xdabnormal@gmail.com>
2023-04-07 16:11:23 +0300

    added ToolType

---
+++ TelegramContest/DrawingModule/ToolType.swift
@@ -0,0 +1,15 @@
+//
+//  ToolType.swift
+//  TelegramContest
+//
+//  Created by N S on 07.04.2023.
+//
+
+import Foundation
+enum ToolType {
+    case eraser
+    case neon
+    case pen
+    case pencil
+    case brush
+}
