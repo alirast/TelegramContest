4fa9baa9138e440dc4bfe03325b8022cf2cded04
alirast <xdabnormal@gmail.com>
2023-05-11 18:15:44 +0300

    WIP: setup TextView

--- TelegramContest/DrawingModule/Text/View/TextView.swift
+++ TelegramContest/DrawingModule/Text/View/TextView.swift
@@ -8,62 +8,42 @@
 import UIKit
 
 class TextView: UIView {
-    lazy var textViewContainer: UIView = {
-        let container = UIView()
-        container.translatesAutoresizingMaskIntoConstraints = false
-        container.frame = frame //the size of container?
-        container.backgroundColor = .clear
-        container.isUserInteractionEnabled = true
-        return container
-    }()
-    
     lazy var textView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
-        //where is the content placed
-        textView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
-        textView.font = UIFont.systemFont(ofSize: 17)
-        
-        textView.tintColor = .white
-        textView.keyboardType = .default
-        textView.keyboardAppearance = .dark
-        textView.spellCheckingType = .no
-        textView.autocorrectionType = .no
-        textView.returnKeyType = .done
-        textView.text = "" //textViewPlaceString
-        textView.textAlignment = .center
-        textView.backgroundColor = .gray //should be clear at the end
-        textView.isUserInteractionEnabled = true
+        textView.backgroundColor = .clear
+        textView.text = "Some text"
+        textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        textView.font = UIFont.systemFont(ofSize: 20)
         return textView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(_ :)), name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(_ :)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        
-        //textViewContainer.addSubview(textView)
-        //addSubview(textViewContainer)
         addSubview(textView)
         
         NSLayoutConstraint.activate([
-            textView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            textView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            textView.topAnchor.constraint(equalTo: topAnchor),
+            textView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            textView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        textView.resignFirstResponder()
-    }
-    
-    @objc private func updateTextView(_ notification: Notification) {
-        let userInfo = notification.userInfo
-        //all coordinations for keyboard
-        let keyboardRectangle = (userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        let keyboardFrame = convert(keyboardRectangle, to: window)
         
+        setupNotificationCenter()
+    }
+    
+
+    
+    private func setupNotificationCenter() {
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func updateTextView(notification: Notification) {
+        let userInfo = notification.userInfo
+        let keyboardRectangle = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardFrame = convert(keyboardRectangle, to: window)
         if notification.name == UIResponder.keyboardWillHideNotification {
             textView.contentInset = UIEdgeInsets.zero
         } else {
@@ -76,4 +56,5 @@ class TextView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+   
 }
