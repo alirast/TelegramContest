19e7420d087762c65b118eba791edf0104d2f37b
alirast <xdabnormal@gmail.com>
2023-05-26 13:54:48 +0300

    fixed tool's width context menu

--- TelegramContest/DrawingModule/Extensions/DrawingVCToolWidth.swift
+++ TelegramContest/DrawingModule/Extensions/DrawingVCToolWidth.swift
@@ -15,21 +15,21 @@ extension DrawingViewController: UIContextMenuInteractionDelegate {
     func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
         
         let configuration = UIContextMenuConfiguration(identifier: nil, previewProvider: nil, actionProvider: { _ in
-            let thin = UIAction(image: UIImage(named: Width.thin.rawValue)) { [self] action in
+            let thin = UIAction(title: "Thin", image: UIImage(named: Width.thin.rawValue)) { [self] action in
                 print("thin chosen")
                 toolWidth = 5.0
                 setupWidth(toolWidth)
                 print("configuration \(toolWidth)")
             }
             
-            let medium = UIAction(image: UIImage(named: Width.medium.rawValue)) { [self] action in
+            let medium = UIAction(title: "Medium", image: UIImage(named: Width.medium.rawValue)) { [self] action in
                 print("medium chosen")
                 toolWidth = 30.0
                 setupWidth(toolWidth)
                 print("configuration \(toolWidth)")
             }
             
-            let bold = UIAction(image: UIImage(named: Width.bold.rawValue)) { [self] action in
+            let bold = UIAction(title: "Bold", image: UIImage(named: Width.bold.rawValue)) { [self] action in
                 print("bold chosen")
                 toolWidth = 200.0
                 setupWidth(self.toolWidth)
@@ -39,7 +39,7 @@ extension DrawingViewController: UIContextMenuInteractionDelegate {
             if #available(iOS 16.0, *) {
                 return UIMenu(options: .displayInline, preferredElementSize: .medium, children: [thin, medium, bold])
             } else {
-                return UIMenu(options: .displayInline, children: [thin, medium, bold])
+                return UIMenu(title: "Width", children: [thin, medium, bold])
             }
         })
 
