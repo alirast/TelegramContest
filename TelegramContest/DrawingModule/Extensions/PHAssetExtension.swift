3197205a7aaca23e560074dc445c87660279b1ba
alirast <xdabnormal@gmail.com>
2023-05-30 14:26:46 +0300

    removed comments & checked retain cycles

--- TelegramContest/DrawingModule/Extensions/PHAssetExtension.swift
+++ TelegramContest/DrawingModule/Extensions/PHAssetExtension.swift
@@ -11,7 +11,6 @@ import Photos
 extension PHAsset {
     func convertToImage(completion: @escaping (_ image: UIImage?) -> Void) {
         let manager = PHImageManager.default()
-//FIXME: - targetSize
         manager.requestImage(for: self, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFill, options: nil) { (image, _) in
             completion(image)
         }
